de5ca80bc3e8fac4d29a4c32a4b9ad559dd15239
Fernando Vedovatto <f.vedovatto@ibm.com>
2022-07-22 15:43:02 -0300

    classes ItemPedido, Pedido, Produto

---
+++ src/main/java/com/ibm/associatescodechallenge/model/Produto.java
@@ -0,0 +1,75 @@
+package com.ibm.associatescodechallenge.model;
+
+import java.util.Objects;
+
+public class Produto {
+
+    private Long id;
+    private String name;
+    private String marca;
+    private Double preco;
+
+    public Produto(){}
+
+    public Produto(Long id, String name, String marca, Double preco) {
+        this.id = id;
+        this.name = name;
+        this.marca = marca;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marca='" + marca + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
