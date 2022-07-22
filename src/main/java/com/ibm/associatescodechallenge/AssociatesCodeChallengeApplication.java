3744796b7320a936607ec1907b76f239abf764d6
Paulo Vieira <paulo.vieira@ibm.com>
2022-07-22 12:26:17 -0300

    Commit Inicial

---
+++ src/main/java/com/ibm/associatescodechallenge/AssociatesCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.associatescodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociatesCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssociatesCodeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibm/associatescodechallenge/AssociatesCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.associatescodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociatesCodeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
