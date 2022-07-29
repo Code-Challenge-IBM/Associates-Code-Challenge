d45e42acad3be02b0823107a7368c5c61c7c0669
Katherine-Martins <katherine.martins@ibm.com>
2022-07-29 13:57:09 -0400

    adicionando Enum ItemStatus

---
+++ src/main/java/com/ibm/associatescodechallenge/enums/ItemStatus.java
@@ -0,0 +1,25 @@
+package com.ibm.associatescodechallenge.enums;
+
+/*
+ *
+*/
+
+public enum ItemStatus {
+    PAGAMENTO_PENDENTE (0),
+    PROCESSANDO (1),
+    APROVADO ( 2),
+    ENVIADO (3),
+    ENTREGUE (4),
+    REJEITADO (5)
+    ;
+
+    private final int valor;
+
+    ItemStatus(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+}
